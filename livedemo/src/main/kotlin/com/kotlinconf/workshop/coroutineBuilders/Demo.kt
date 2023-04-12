bd1011f84bbd009c889053e44957bfa57bdf72ae
Daniel Oskarsson <daniel.oskarsson@gmail.com>
2023-04-12 12:33:12 +0200

    Add coroutineContext info to Demo

--- livedemo/src/main/kotlin/com/kotlinconf/workshop/coroutineBuilders/Demo.kt
+++ livedemo/src/main/kotlin/com/kotlinconf/workshop/coroutineBuilders/Demo.kt
@@ -3,8 +3,27 @@ package com.kotlinconf.workshop.coroutineBuilders
 import com.kotlinconf.workshop.network.Order
 import com.kotlinconf.workshop.util.log
 import kotlinx.coroutines.*
+import kotlin.coroutines.coroutineContext
 
-fun main() {
+fun main() = runBlocking<Unit> {
+    whichContextDoIHave()
+    println("RunBlocking context: $coroutineContext")
+    launch(Dispatchers.Default + CoroutineName("my")) {
+        whichContextDoIHave()
+        println("Launch parent context: $coroutineContext")
+        launch(Dispatchers.IO) {
+            whichContextDoIHave()
+            println("Launch child context: $coroutineContext")
+        }
+    }
+}
+
+suspend fun whichContextDoIHave() {
+//    launch() Does not work. Scope required.
+    println(coroutineContext)
+}
+
+fun main2() {
     runBlocking(Dispatchers.Default) {
         val orders = loadOrders()
         log(orders)
