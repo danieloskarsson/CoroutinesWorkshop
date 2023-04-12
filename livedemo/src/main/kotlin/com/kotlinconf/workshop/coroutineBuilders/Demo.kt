7191cc840cb68524f145961d69a38fabf029f83a
Daniel Oskarsson <daniel.oskarsson@gmail.com>
2023-04-12 11:18:36 +0200

    Extract loadOrders() in Demo

--- livedemo/src/main/kotlin/com/kotlinconf/workshop/coroutineBuilders/Demo.kt
+++ livedemo/src/main/kotlin/com/kotlinconf/workshop/coroutineBuilders/Demo.kt
@@ -1,18 +1,21 @@
 package com.kotlinconf.workshop.coroutineBuilders
 
+import com.kotlinconf.workshop.network.Order
 import com.kotlinconf.workshop.util.log
-import kotlinx.coroutines.CoroutineName
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 fun main() {
     runBlocking(Dispatchers.Default) {
-        val userId = async(CoroutineName("userId")) {
-            service.login("user", "1234")
-        }
-        val shopInfo = async { service.loadShopInfo() }
-        val orders = service.loadOrders(userId.await(), shopInfo.await())
+        val orders = loadOrders()
         log(orders)
     }
+}
+
+suspend fun loadOrders(): List<Order> = coroutineScope {
+    val userId = async(CoroutineName("userId")) {
+        service.login("user", "1234")
+    }
+    val shopInfo = async { service.loadShopInfo() }
+    val orders = service.loadOrders(userId.await(), shopInfo.await())
+    orders
 }
