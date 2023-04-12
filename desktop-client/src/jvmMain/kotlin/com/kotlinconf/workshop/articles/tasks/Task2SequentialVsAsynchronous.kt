2a3afd1cae701a0654978838b435631a027186c2
Daniel Oskarsson <daniel.oskarsson@gmail.com>
2023-04-12 10:31:37 +0200

    Task2

--- desktop-client/src/jvmMain/kotlin/com/kotlinconf/workshop/articles/tasks/Task2SequentialVsAsynchronous.kt
+++ desktop-client/src/jvmMain/kotlin/com/kotlinconf/workshop/articles/tasks/Task2SequentialVsAsynchronous.kt
@@ -5,8 +5,36 @@ import com.kotlinconf.workshop.articles.network.BlogService
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 // Task: Implement concurrent loading of articles
 suspend fun loadArticlesConcurrently(service: BlogService): List<Article> = coroutineScope {
-    TODO()
+    service.getArticleInfoList().map {
+        async {
+            Article(it, service.getComments(it))
+        }
+    }.awaitAll()
 }
+
+suspend fun similarSolution(service: BlogService): List<Article> = coroutineScope {
+    service.getArticleInfoList().map {
+        async {
+            Article(it, service.getComments(it))
+        }
+    }.map {
+        it.await()
+    }
+}
+
+suspend fun firstSolution(service: BlogService): List<Article> = coroutineScope {
+    val articleInfoList = service.getArticleInfoList()
+    val articles = mutableListOf<Article>()
+    articleInfoList.forEach {
+        launch {
+            val comments = service.getComments(it)
+            articles.add(Article(it, comments))
+        }
+    }
+    return@coroutineScope articles
+}
+
